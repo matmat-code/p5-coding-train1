119966d8ae6f798654c88689f78ea07269e730ac
Deevak <deevaklaxmikaant@gmail.com>
2020-02-08 18:01:55 +0800

    close to finishing

--- 7.4.js
+++ 7.4.js
@@ -1,17 +1,25 @@
 let bublles = []
 
 function setup(){
-    createCanvas(600,400)
+    createCanvas(600,400);
+    let x = random(width);
+    let y = random(height);
+    let r = random(10,50);
+    let b = new Bubble(x,y,r);
+    bublles.push(b);
 }
 
 function mousePressed(){
-    let r = random(10,50);
-    let b = new Bubble(mouseX,mouseY,r);
-    bublles.push(b);
+    for (let i = 0; i < bubbles.length; i++){
+        bublles[i].move();
+        bublles[i].show();
+    }
 }
 
 function draw(){
     background(0);
+    bubble.move();
+    bubble.show();
     for (let i = 0; i < bubbles.length; i++){
         bublles[i].move();
         bublles[i].show();
@@ -25,6 +33,12 @@ class Bubble {
         this.r = r;
     }
 
+    clicked(){
+        let d = dist(mouseX,mouseY,this.x,this.y);
+        if (d < this.r);
+        console.log("CLICKED ON BUBBLE");
+    }
+
     move(){
         this.x = this.x + random(-15,15);
         this.y = this.y + random(-15,15);
@@ -34,6 +48,6 @@ class Bubble {
         stroke(255);
         strokeWeight(4);
         noFill();
-        ellipse( this.x,this.y,this.r * 2)
+        ellipse( this.x,this.y,this.r * 2);
     }
 }
